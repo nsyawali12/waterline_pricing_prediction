238c8161874b07d85066876fa87fb5cd1b8e07c7
nsyawali12 <nsyawali12@gmail.com>
2020-09-28 02:13:37 +0700

    progress harga pipa

--- waterline_v1.py
+++ waterline_v1.py
@@ -6,29 +6,33 @@ def rukun_warga(rt):
   rw += rw
   return rw
 
-def pengeboran(max_distance, pengeboran_per_meter, pipe_price):
-  res_drill = (max_distance * pengeboran_per_meter) + (max_distance * pipe_price)
+def pengeboran(max_distance, pengeboran_per_meter, price):
+  res_drill = (max_distance * pengeboran_per_meter) + (max_distance * price)
   return res_drill
 
 def pompa(pump_price):
   res_pompa = 1*pump_price
   return res_pompa 
 
-def harga_pipa_tampungan(d_to_rt, pipe_price): #pipe price disini pipa kelas rt
-  res_pipa_tampungan = d_to_rt  * pipe_price # jarak ke RT x harga kelas pipa rt
+def harga_pipa_tampungan(d_to_rt, price): #pipe price disini pipa kelas rt
+  res_pipa_tampungan = d_to_rt  * price # jarak ke RT x harga kelas pipa rt
   return res_pipa_tampungan
 
 def tampungan(res_pipa_tampungan):
   total_tampungan = res_pipa_tampungan #total semua dari pipa tampungan
   return total_tampungan
 
-def utamaRW(pipe_price):
-  tamp_rw = 1 * pipe_price # 1 x harga tampungan 
+def utamaRW(price):
+  tamp_rw = 1 * price # 1 x harga tampungan 
   return tamp_rw
 
-def sekunderRT(rt, pipe_price):
-  tamp_rt = 1 * pipe_price # 1 x harga tampungan pipa rt
+def sekunderRT(rt, price):
+  tamp_rt = 1 * price # 1 x harga tampungan pipa rt
   return tamp_rt
+
+def allprice(price):
+  all_p =+ price
+  return all_p
 
 # no_rw = 0 #variable no rw
 # jum_rt = 0 #variable jumlah rt
@@ -44,10 +48,36 @@ print("----------------------------")
 print("----Input Several Valeus----")
 no_rw = input("Nomor RW: ")
 jum_rt = input("Jumlah RT: ")
-kk = input("Kepala Keluarga: ")
+kk = int(input("Kepala Keluarga: "))
 s_min = input("Kedalaman Sumur Minimal: ")
 s_max = input("Kedalaman Sumur Maksimal: ")
 water_s = input("Status Kekurangan Air (Y/N): ")
 
+## Jangkauan layanan = Jumlah kapasitas kepala keluarga
+jangkauan = kk 
+
+## PRicing dari database jumlah KK
+print('Pricing')
+if (kk >= 0) and (kk <= 200):
+  print('Pipa 0.5m')
+  pipe_price = 2000
+  print('tampungan 1x1m')
+  t_price = 500000
+  
+
+elif (kk > 200) and (kk <= 600):
+  print('Pipa 1m')
+  price = 4000
+  
+elif (kk > 600) and (kk<1000):
+  print('Pipa 1.5')
+  price = 6000
 
 
+
+
+
+print('Output Prediksi Harga')
+
+print("Lokasi Sumur: ", no_rw)
+print("Jangkauan Layanan: ", jangkauan)
